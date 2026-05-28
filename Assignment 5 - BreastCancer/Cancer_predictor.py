dc0726d3fb74257e5c52eb5b9c2e9cda78b19368
ALPHIN JOSEPH ALEX <alphwolf10@gmail.com>
2026-05-28 17:54:35 +0530

    Edit
    Added conditional statements to display diagnosis based on prediction.

--- Assignment 5 - BreastCancer/Cancer_predictor.py
+++ Assignment 5 - BreastCancer/Cancer_predictor.py
@@ -3,3 +3,7 @@ model = joblib.load("model.pkl")
 input_data = [[1,323,424,546,646,646,78,979,800,900,100,200,300,400,500,600,700,800,900,1000,1100,1200,1300,1400,1500,1600,1700,1800,1900,2000]]
 prediction = model.predict(input_data)
 print("Prediction for the input data:", prediction)
+if prediction[0] == 1:
+    print("The predicted diagnosis is: Malignant")
+else:
+    print("The predicted diagnosis is: Benign")
